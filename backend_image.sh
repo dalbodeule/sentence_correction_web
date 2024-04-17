4b1cc021d587455315ab48444da9637cf2adc8d4
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-04-17 12:40:54 +0900

    add docker build scripts

---
+++ backend_image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 현재 날짜와 시간을 YYMMDD(AM/PM)HHMM 형식으로 설정
+current_time=$(date +'%y%m%d%p%H%M')
+
+# Docker 이미지 빌드 명령 실행
+docker build --no-cache -t sentence_correction_backend:$current_time .
