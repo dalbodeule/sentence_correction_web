276a3421e8e98909c6432ca05285e8565a0c12f5
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-04-17 20:42:40 +0900

    add github login, add naver login(ready, but api isn't ready.)

--- backend/router_auth.py
+++ backend/router_auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Security, HTTPException
 from fastapi.security import APIKeyCookie
-from fastapi_sso import GoogleSSO
+from fastapi_sso import GoogleSSO, NaverSSO, GithubSSO
 from jose import jwt
 from datetime import datetime, timedelta
 
@@ -8,7 +8,8 @@ from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import RedirectResponse
 
-from backend.config import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL, SECRET_KEY, FRONTEND_URL
+from backend.config import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL, SECRET_KEY, FRONTEND_URL, NAVER_CLIENT_ID, \
+    NAVER_CLIENT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET
 from backend.models.Blacklist import add_blacklist, find_blacklist
 from backend.models.User import create_or_update_user
 
@@ -17,8 +18,20 @@ router = APIRouter(prefix='/login', tags=['auth'])
 google_sso = GoogleSSO(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
-    f'{BASE_URL}/login/google/callback')
+    f'{BASE_URL}/login/google/callback'
+)
+naver_sso = NaverSSO(
+    client_id=NAVER_CLIENT_ID,
+    client_secret=NAVER_CLIENT_SECRET,
+    redirect_uri=f'{BASE_URL}/login/naver/callback',
+    allow_insecure_http=True
+)
 
+github_sso = GithubSSO(
+    GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET,
+    f'{BASE_URL}/login/github/callback'
+)
 
 class Session(BaseModel):
     name: str
@@ -86,3 +99,68 @@ async def login_google_callback(request: Request):
         )
 
         return response
+
+@router.get("/github", include_in_schema=False)
+async def login_github(request: Request):
+    with github_sso:
+        return await github_sso.get_login_redirect()
+
+
+@router.get("/github/callback", include_in_schema=False)
+async def login_github_callback(request: Request):
+    with github_sso:
+        user = await github_sso.verify_and_process(request)
+        if not user:
+            raise HTTPException(status_code=401, detail="Authentication failed")
+
+        userdata = await create_or_update_user(
+            vendor="github",
+            token=user.id,  # `user.id`는 Google에서 제공하는 사용자 ID입니다.
+            profile=user.picture,  # 사용자의 프로필 이미지 URL입니다.
+            name=user.display_name,
+            email=user.email
+        )
+        exp = datetime.now() + timedelta(hours=1)
+
+        token = jwt.encode({"pld": Session(**userdata).__dict__, "exp": exp, "sub": user.email}, key=SECRET_KEY,
+                           algorithm="HS256")
+        response = RedirectResponse(url=FRONTEND_URL, status_code=302)
+        response.set_cookie(
+            key="token", value=token, httponly=True
+        )
+
+        return response
+
+"""
+naver login errors.
+@router.get("/naver", include_in_schema=False)
+async def login_naver(request: Request):
+    with naver_sso:
+        return await naver_sso.get_login_redirect()
+
+
+@router.get("/naver/callback", include_in_schema=False)
+async def login_naver_callback(request: Request):
+    with naver_sso:
+        user = await naver_sso.verify_and_process(request, params={"client_secret": NAVER_CLIENT_SECRET})
+        if not user:
+            raise HTTPException(status_code=401, detail="Authentication failed")
+        return user
+        userdata = await create_or_update_user(
+            vendor="naver",
+            token=user.id,  # `user.id`는 Google에서 제공하는 사용자 ID입니다.
+            profile=user.picture,  # 사용자의 프로필 이미지 URL입니다.
+            name=user.display_name,
+            email=user.email
+        )
+        exp = datetime.now() + timedelta(hours=1)
+
+        token = jwt.encode({"pld": Session(**userdata).__dict__, "exp": exp, "sub": user.email}, key=SECRET_KEY,
+                           algorithm="HS256")
+        response = RedirectResponse(url=FRONTEND_URL, status_code=302)
+        response.set_cookie(
+            key="token", value=token, httponly=True
+        )
+
+        return response
+"""
--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -9,8 +9,10 @@ useHead({
   title: '맞춤법 검사기 :: 로그인'
 })
 
-const methods: Array<{url: string, display: string, vendor: string}> = [
-  { url: `${config.public.backendUrl}/login/google`, display: 'Google로 로그인', vendor: 'google'},
+const methods: Array<{url: string, display: string, id: string, color: string, bgcolor: string, icon: boolean}> = [
+  { url: `${config.public.backendUrl}/login/google`, display: 'Google 로그인', id: 'google', icon: true, color: '#444', bgcolor: '#ffffff'},
+//  { url: `${config.public.backendUrl}/login/naver`, display: 'Naver 로그인', id: 'naver', icon: false, color: '#ffffff', bgcolor: '#03c75a'},
+  { url: `${config.public.backendUrl}/login/github`, display: 'Github 로그인', id: 'github', icon: true, color: '#333', bgcolor: '#fafafa'},
 ]
 
 if (auth.authenticated) {
@@ -19,13 +21,14 @@ if (auth.authenticated) {
 </script>
 
 <template>
-  <div class="container">
-    <div class="box columns is-vcentered" style="min-height: 80vh">
-      <div v-for="method in methods" class="has-text-centered column">
-        <a v-bind:href="method.url" class="button is-align-self-center">
-          <font-awesome-icon :icon="['fab', method.vendor]" />&nbsp; {{ method.display }}
-        </a>
-      </div>
+  <div class="container box" style="min-height: 80vh">
+    <div v-for="method in methods" style="margin-left: auto; margin-top: 20px;">
+      <a v-if="method.icon" v-bind:href="method.url" class="button is-align-self-center" v-bind:style="`background: ${method.bgcolor}; color: ${method.color}; width: 180px;`">
+        <font-awesome-icon :icon="['fab', method.id]"/>&nbsp; {{ method.display }}
+      </a>
+      <a v-else v-bind:href="method.url" class="button is-align-self-center" v-bind:style="`background: ${method.bgcolor}; color: ${method.color}; width: 180px;`">
+        <img v-bind:src="`/${method.id}-logo.png`" alt="" style="height: 20px"/>&nbsp; {{ method.display }}
+      </a>
     </div>
   </div>
 </template>
