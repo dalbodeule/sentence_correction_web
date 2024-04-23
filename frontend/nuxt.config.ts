5c59696c884d2568a91179b506cdb0e48061ee67
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-04-23 15:13:56 +0900

    dockerfile changed.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
     ],
     build: {
       rollupOptions: {
-        external: ['Diff']
+        external: ['Diff', 'ts-invariant/process']
       }
     }
   },
